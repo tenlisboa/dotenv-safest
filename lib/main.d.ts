6441e1bc60c9823671d8aa6107b42d45c42d20c3
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2023-07-08 18:37:08 -0300

    fix: Update type

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,9 +1,22 @@
-import type { DotenvConfigOutput, DotenvOptions } from 'dotenv';
+import type { DotenvConfigOutput, DotenvConfigOptions } from 'dotenv';
 
-type DotenvSafeOptions = DotenvOptions & {
+export type DotenvSafeOptions = DotenvConfigOptions & {
+    /**
+     * Default: false
+     * If true, the env should be defined in the .env file, but can be empty.
+     */
     allowEmptyValues?: boolean;
+    /**
+     * Default: .env.example
+     * The path to the example environment file.
+     */
     example?: string;
-    path?: string;
+    /**
+     * Default: .env.example
+     * The path to the example environment file.
+     * @deprecated Use example instead.
+     * @alias example
+     */
     sample?: string;
 };
 
